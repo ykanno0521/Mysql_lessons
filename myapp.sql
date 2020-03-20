e18626157e13affde18dce0bc7174049872c1e08
vagrant <vagrant@localhost.localdomain>
2020-03-20 12:30:53 +0900

    insert record

--- myapp.sql
+++ myapp.sql
@@ -12,3 +12,11 @@ create table users (
   name varchar(20),
   score float
 );
+
+insert into users (id, name, score) values
+(1, 'taguchi', 5.8),
+(2, 'www', 5.8),
+(3, 'yamada', null),
+(3, 'tanaka', 6.1);
+
+select * from users;
