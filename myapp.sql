3c740e1a3426daa65c94e710ad74e5ee4147985a
vagrant <vagrant@localhost.localdomain>
2020-03-20 13:00:57 +0900

    レコード抽出

--- myapp.sql
+++ myapp.sql
@@ -11,16 +11,19 @@ create table users (
   id int unsigned primary key auto_increment,
   name varchar(20) unique,
   -- score float not null
-  score float default 0.0
+  score float
 );
 
--- alter table users add column email varchar(255);
-alter table users add column email varchar(255) after name;
-alter table users drop column score;
-alter table users change name user_name varchar(100) default 'nobody';
+insert into users (name, score) values ('taguchi', 5.8);
+insert into users (name, score) values ('yamda', 7.0);
+insert into users (name, score) values ('tanaka', 5.2);
+insert into users (name, score) values ('hashimoto', 2.2);
+insert into users (name, score) values ('kikuchi', 5.0);
+insert into users (name, score) values ('honda', 4.4);
 
-desc users;
+-- select * from users;
+-- select id, name from users;
+-- select * from users where score >= 6.0;
+-- select * from users where score >= 3.0 and score <= 6.0;
 
-alter table users rename persons;
-show tables;
-drop table if exists persons;
+select * from users;
