8192be5786fe34068714796bb10562f5a6155ad6
vagrant <vagrant@localhost.localdomain>
2020-03-20 12:50:37 +0900

    カラム、テーブル操作

--- myapp.sql
+++ myapp.sql
@@ -8,15 +8,19 @@ number:
 
 drop table if exists users;
 create table users (
-  id int unsigned,
-  name varchar(20),
-  score float
+  id int unsigned primary key auto_increment,
+  name varchar(20) unique,
+  -- score float not null
+  score float default 0.0
 );
 
-insert into users (id, name, score) values
-(1, 'taguchi', 5.8),
-(2, 'www', 5.8),
-(3, 'yamada', null),
-(3, 'tanaka', 6.1);
+-- alter table users add column email varchar(255);
+alter table users add column email varchar(255) after name;
+alter table users drop column score;
+alter table users change name user_name varchar(100) default 'nobody';
 
-select * from users;
+desc users;
+
+alter table users rename persons;
+show tables;
+drop table if exists persons;
